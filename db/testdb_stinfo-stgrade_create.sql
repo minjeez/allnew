2eb8657775cfad4380e9b4cf17012fac5888b78b
hmj0094 <127065218+minjeez@users.noreply.github.com>
2023-03-24 15:07:36 +0900

    jebal

--- db/testdb_stinfo-stgrade_create.sql
+++ db/testdb_stinfo-stgrade_create.sql
@@ -15,16 +15,14 @@ alter table st_info add constraint pk_stinfo primary key (ST_ID);
 alter table st_grade add constraint pk_stgrade primary key (ST_ID);
 
 explain st_info;
-explain st_grade
+explain st_grade;
 
 insert into st_info values (202301, "LeeGilDong", "Game");
 insert into st_info values (202302, "KimGilDong", "Computer");
 insert into st_info values (202303, "HongGilDong", "Game");
 select * from st_info;
- 
+
 insert into st_grade values (202301, 80, 70);
 insert into st_grade values (202302, 90, 75);
 insert into st_grade values (202303, 95, 85);
 select * from st_grade;
-
-
